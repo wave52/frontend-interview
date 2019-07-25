d94ad21ce38e5a731df0db5ca2c1b26a7491f7da
WuChengran <chengran.wu@56qq.com>
2019-07-25 18:05:22 +0800

    add async question

---
+++ implement/promise.js
@@ -0,0 +1,9 @@
+// 实现 promise TODO:
+
+// Promise A+ 规范：https://promisesaplus.com/
+// Promise A+ 规范中文：http://malcolmyu.github.io/malnote/2015/06/12/Promises-A-Plus/
+
+// https://github.com/then/promise
+// https://github.com/taylorhakes/promise-polyfill
+
+// 教程：https://yq.aliyun.com/articles/613412
