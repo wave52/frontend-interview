d9578a530946d1b78c2bb23fae379bc273932f45
WuChengran <chengran.wu@56qq.com>
2019-08-01 18:09:09 +0800

    add some question

---
+++ implement/convert.js
@@ -0,0 +1,73 @@
+// 实现 convert 方法，把原始 list 转换成树形结构，要求尽可能降低时间复杂度
+
+// 原始 list 如下
+let list = [
+  { id: 1, name: '部门A', parentId: 0 },
+  { id: 2, name: '部门B', parentId: 0 },
+  { id: 3, name: '部门C', parentId: 1 },
+  { id: 4, name: '部门D', parentId: 1 },
+  { id: 5, name: '部门E', parentId: 2 },
+  { id: 6, name: '部门F', parentId: 3 },
+  { id: 7, name: '部门G', parentId: 2 },
+  { id: 8, name: '部门H', parentId: 4 },
+];
+
+/*
+转换后的结果如下
+let result = [
+  {
+    id: 1,
+    name: '部门A',
+    parentId: 0,
+    children: [
+      {
+        id: 3,
+        name: '部门C',
+        parentId: 1,
+        children: [
+          {
+            id: 6,
+            name: '部门F',
+            parentId: 3,
+          },
+          {
+            id: 16,
+            name: '部门L',
+            parentId: 3,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: '部门D',
+        parentId: 1,
+        children: [
+          {
+            id: 8,
+            name: '部门H',
+            parentId: 4,
+          },
+        ],
+      },
+    ],
+  },
+  ...
+]; */
+
+// 1
+function convert(list) {
+  var map = {}
+  return list.reduce((total, current) => {
+    map[current.id] = current;
+    if (current.parentId === 0) {
+      total.push(current)
+    } else {
+      var parent = map[current.parentId]      
+      if(!parent.children) parent.children = []
+      parent.children.push(current)
+    }
+    return total
+  }, []);
+}
+
+console.log(JSON.stringify(convert(list)));
---
+++ implement/moveZero.js
@@ -0,0 +1,46 @@
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+// 示例
+// 输入: [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+// 我的
+function moveZero(arr) {
+  let i = 0;
+  let j = 0; // 记录0个数
+  while (i < arr.length) {
+    if (arr[i] === 0) {
+      j++;
+      arr.splice(i, 1);
+      if (arr[i] === 0) {
+        // 解决连续0
+        i = i;
+        continue;
+      }
+    }
+    i++;
+  }
+  arr.push(...new Array(j).fill(0));
+  return arr;
+}
+
+console.log(moveZero([0, 1, 0, 3, 12]));
+console.log(moveZero([0, 0, 1, 3, 12]));
+
+// 网上比较好的
+// 设定一个慢指针一个快指针，快指针每次+1， 当慢指针的值不等于0的时候也往后移动，当慢指针等于0并且快指针不等于0的时候，交换快慢指针的值，慢指针再+1
+function moveZero2(arr) {
+  let i = 0;
+  let j = 0;
+  while (j < arr.length) {
+    if (arr[i] !== 0) {
+      i++;
+    } else if (arr[j] !== 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+    }
+    j++;
+  }
+  return arr
+}
+
+console.log(moveZero2([0, 1, 0, 3, 12]));
+console.log(moveZero2([0, 0, 1, 3, 12]));
