f74f02dfa12acb106d725610aa4592272b321b6d
WuChengran <chengran.wu@56qq.com>
2019-11-06 10:59:54 +0800

    add instanceof implement

---
+++ implement/instanceof/instanceof.js
@@ -0,0 +1,14 @@
+function instanceof(left, right) {
+  // 获得类型的原型
+  let prototype = right.prototype
+  // 获得对象的原型
+  left = left.__proto__
+  // 判断对象的类型是否等于类型的原型
+  while (true) {
+    if (left === null)
+      return false
+    if (prototype === left)
+      return true
+    left = left.__proto__
+  }
+}
