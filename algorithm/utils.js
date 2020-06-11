fd46c6a90f3f97e68c0e013c68d7cf591d643640
wave52 <7003397@qq.com>
2020-06-12 02:18:39 +0800

    add

---
+++ algorithm/utils.js
@@ -0,0 +1,12 @@
+// 做算法题时，常用的工具方法或步骤
+// es6 为主
+
+// 快速创建 n*n 的二维数组
+const dp = Array.from({ length: n }, () => new Array(n).fill(0));
+
+// 交换两个数
+[a, b] = [b, a];
+
+// 求最值
+Math.max(...arr)
+Math.min(...arr)
